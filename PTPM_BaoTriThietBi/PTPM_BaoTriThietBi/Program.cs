adefe63e80568bd22110a4efa2bcdae4d39a9acf
nger111 <huy665493@gmail.com>
2025-10-14 23:49:51 +0700

    ver1

---
+++ PTPM_BaoTriThietBi/PTPM_BaoTriThietBi/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTPM_BaoTriThietBi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 🔸 Thêm cấu hình DbContext - kết nối tới SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QLBaoTriBaoHanhDB")));
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
