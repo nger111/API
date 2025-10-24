792128b9622d274a6172d6974b224eac4bc1d8dd
Duong191 <tungduong19012005@gmail.com>
2025-10-24 22:52:31 +0700

    Big Update

--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff1f18602e96ac9aa1f3897fbaffa0fe12ece25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aaa174d66ccef4ff8eee70fc3e9565adf68fe67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
